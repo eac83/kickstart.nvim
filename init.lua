776d8e05ca36f1881e2fd530d0783d42a512ad80
Ewan Chamberlain <ewanachamberlain@gmail.com>
2024-07-11 19:15:13 +0100

    added undotree

--- init.lua
+++ init.lua
@@ -921,3 +921,18 @@ vim.g.vimtex_view_method = 'zathura'
 
 -- Compiler
 vim.g.vimtex_compiler_method = 'latexmk'
+
+-- Undotree
+vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
+if vim.fn.has 'persistent_undo' == 1 then
+  local target_path = vim.fn.expand '~/.undodir'
+
+  -- create the directory and any parent directories
+  -- if the location does not exist.
+  if vim.fn.isdirectory(target_path) == 0 then
+    vim.fn.mkdir(target_path, 'p', 0700)
+  end
+
+  vim.opt.undodir = target_path
+  vim.opt.undofile = true
+end
